91ccd323b0fea775f27c1a9e42691d3192340700
nicusorfrancu98-boop <nicusorfrancu98@gmail.com>
2025-08-07 15:24:55 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,135 +1,126 @@
-const mineflayer = require('mineflayer')
-const Movements = require('mineflayer-pathfinder').Movements
-const pathfinder = require('mineflayer-pathfinder').pathfinder
-const { GoalBlock} = require('mineflayer-pathfinder').goals
+const path = require('path');
+const fs = require('fs');
+
+const settingsPath = path.resolve(__dirname, 'settings.json');
+
+console.log('[DEBUG] Loading config from:', settingsPath);
+
+let config;
+try {
+  config = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
+} catch (err) {
+  console.error('[ERROR] Failed to load settings.json! Error:', err);
+  process.exit(1);
+}
+
+const mineflayer = require('mineflayer');
+const Movements = require('mineflayer-pathfinder').Movements;
+const pathfinder = require('mineflayer-pathfinder').pathfinder;
+const { GoalBlock } = require('mineflayer-pathfinder').goals;
 const antiafk = require("mineflayer-antiafk");
-const pvp = require('mineflayer-pvp').plugin
-
-var http = require('http');  
-http.createServer(function (req, res) {   
-  res.write("I'm alive");   
-  res.end(); 
-}).listen(8080);
-
-const config = require('./settings.json');
+const pvp = require('mineflayer-pvp').plugin;
 
 function createBot () {
   const bot = mineflayer.createBot({
-      username: config['bot-account']['username'],
-      password: config['bot-account']['password'],
+      username: config.bot.username,
+      password: config.bot.password,
       host: config.server.ip,
       port: config.server.port,
       version: config.server.version
-  })
+  });
 
-  bot.loadPlugin(pathfinder)
-  const mcData = require('minecraft-data')(bot.version)
-  const defaultMove = new Movements(bot, mcData)
-  bot.settings.colorsEnabled = false
+  bot.loadPlugin(pathfinder);
+  bot.loadPlugin(pvp);
+  bot.loadPlugin(antiafk);
 
-  bot.once("spawn", function(){
-      console.log("\x1b[33m[BotLog] Bot joined to the server", '\x1b[0m')
+  const mcData = require('minecraft-data')(bot.version);
+  const defaultMove = new Movements(bot, mcData);
 
-      if(config.utils['auto-auth'].enabled){
-        console.log("[INFO] Started auto-auth module")
+  bot.once("spawn", () => {
+      console.log("\x1b[33m[BotLog] Bot joined the server\x1b[0m");
 
-          var password = config.utils['auto-auth'].password
-          setTimeout(function() {
-              bot.chat(`/register ${password} ${password}`)
-              bot.chat(`/login ${password}`)
-          }, 500);
+      if(config.utils["auto-auth"].enabled){
+        console.log("[INFO] Started auto-auth module");
 
-          console.log(`[Auth] Authentification commands executed.`)
+        let password = config.utils["auto-auth"].password;
+        setTimeout(() => {
+          bot.chat(`/register ${password} ${password}`);
+          bot.chat(`/login ${password}`);
+        }, 500);
+
+        console.log(`[Auth] Authentication commands executed.`);
       }
 
-      if(config.utils['chat-messages'].enabled){
-        console.log("[INFO] Started chat-messages module")
-        var messages = config.utils['chat-messages']['messages']
+      if(config.utils["chat-messages"].enabled){
+        console.log("[INFO] Started chat-messages module");
+        let messages = config.utils["chat-messages"].messages;
 
-          if(config.utils['chat-messages'].repeat){
-            var delay = config.utils['chat-messages']['repeat-delay']
-            let i = 0
+        if(config.utils["chat-messages"].repeat){
+          let delay = config.utils["chat-messages"]["repeat-delay"];
+          let i = 0;
 
-            let msg_timer = setInterval(() => {
-                bot.chat(`${messages[i]}`)
-
-                if(i+1 == messages.length){
-                    i = 0
-                } else i++
-            }, delay * 1000)
-          } else {
-              messages.forEach(function(msg){
-                  bot.chat(msg)
-              })
+          setInterval(() => {
+              bot.chat(messages[i]);
+              i = (i + 1) % messages.length;
+          }, delay * 1000);
+        } else {
+          messages.forEach(msg => bot.chat(msg));
         }
       }
-      
 
-      const pos = config.position
-
-      if (config.position.enabled){
-          console.log(`\x1b[32m[BotLog] Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})\x1b[0m`)
-          bot.pathfinder.setMovements(defaultMove)
-          bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z))
+      if(config.position.enabled){
+          let pos = config.position;
+          console.log(`\x1b[32m[BotLog] Moving to target location (${pos.x}, ${pos.y}, ${pos.z})\x1b[0m`);
+          bot.pathfinder.setMovements(defaultMove);
+          bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
       }
-      
-      if(config.utils['anti-afk'].enabled){
-        bot.setControlState('jump', true)
-        if(config.utils['anti-afk'].sneak){
-            bot.setControlState('sneak', true)
+
+      if(config.utils["anti-afk"].enabled){
+        bot.setControlState('jump', true);
+        if(config.utils["anti-afk"].sneak){
+            bot.setControlState('sneak', true);
         }
       }
-  })
+  });
 
-  bot.loadPlugin(pathfinder)
-bot.loadPlugin(pvp)
-
-bot.on('chat', (username, message) => {
-  if (message === 'fight me') {
-    const player = bot.players[username]
-
-    if (!player) {
-      bot.chat("I can't see you.")
-      return
+  bot.on('chat', (username, message) => {
+    if(message === 'fight me'){
+      let player = bot.players[username];
+      if(!player) return bot.chat("I can't see you.");
+      bot.pvp.attack(player.entity);
     }
+    if(message === 'stop'){
+      bot.pvp.stop();
+    }
+  });
 
-    bot.pvp.attack(player.entity)
+  bot.on('chat', (username, message) => {
+    if(config.utils['chat-log']){
+      console.log(`[ChatLog] <${username}> ${message}`);
+    }
+  });
+
+  bot.on("goal_reached", () => {
+      console.log("\x1b[32m[BotLog] Bot arrived to target location.\x1b[0m");
+  });
+
+  bot.on("death", () => {
+      console.log("\x1b[33m[BotLog] Bot died and respawned\x1b[0m");
+  });
+
+  if(config.utils["auto-reconnect"]){
+    bot.on('end', () => {
+      createBot();
+    });
   }
 
-  if (message === 'stop') {
-    bot.pvp.stop()
-  }
-})
+  bot.on('kicked', reason => {
+    console.log('\x1b[33m',`[BotLog] Bot was kicked. Reason:\n${reason}`, '\x1b[0m');
+  });
 
-bot.loadPlugin(antiafk);
-        
- bot.on("spawn", ()=>{
-    bot.afk.start();
-})
-
-  bot.on("chat", function(username, message){
-      if(config.utils['chat-log']){
-          console.log(`[ChatLog] <${username}> ${message}`)
-      }
-  })
-
-  bot.on("goal_reached", function(){
-      console.log(`\x1b[32m[BotLog] Bot arrived to target location. ${bot.entity.position}\x1b[0m`)
-  })
-
-  bot.on("death", function(){
-      console.log(`\x1b[33m[BotLog] Bot has been died and was respawned ${bot.entity.position}`, '\x1b[0m')
-  })
-
-  if(config.utils['auto-reconnect']){
-      bot.on('end', function(){
-        createBot()
-      })
-  }
-
-  bot.on('kicked', (reason) => console.log('\x1b[33m',`[BotLog] Bot was kicked from the server. Reason: \n${reason}`, '\x1b[0m'))
-  bot.on('error', err => console.log(`\x1b[31m[ERROR] ${err.message}`, '\x1b[0m'))
-
+  bot.on('error', err => {
+    console.error(`\x1b[31m[ERROR] ${err.message}\x1b[0m`);
+  });
 }
 
-createBot()
+createBot();
